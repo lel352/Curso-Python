96105982fb9abb2139f6e7f177c18b660e1ab53f
Leandro <leandro@pcsistemas.com.br>
2022-01-18 16:23:36 -0300

    Ajustes

---
+++ aulamodulospython/aula15/aula15.2.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+# Caminho para a raiz do projeto
+ROOT_FOLDER = Path(__file__).parent
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'bin' / 'chromedriver'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(
+        executable_path=CHROME_DRIVER_PATH,
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+
+if __name__ == '__main__':
+    # Example
+    options = ('--disable-gpu', '--no-sandbox',)
+    browser = make_chrome_browser(*options)
+
+    # Como antes
+    browser.get('https://www.google.com')
+
+    input_element = browser.find_element(By.NAME, 'q')
+    input_element.send_keys('Python')
+    sleep(3)
+    input_element.send_keys(Keys.ENTER)
+    sleep(3)
+    browser.quit()
