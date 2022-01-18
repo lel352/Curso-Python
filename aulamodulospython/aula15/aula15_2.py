9ebd5ac333fe6b7c1f1f9fc626cc51c8d0afe88e
Leandro <leandro@pcsistemas.com.br>
2022-01-18 16:49:01 -0300

    Ajustes

--- aulamodulospython/aula15/aula15_2.py
+++ aulamodulospython/aula15/aula15_2.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.keys import Keys
 # Caminho para a raiz do projeto
 ROOT_FOLDER = Path(__file__).parent
 # Caminho para a pasta onde o chromedriver está
-CHROME_DRIVER_PATH = ROOT_FOLDER / 'bin' / 'chromedriver'
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'bin' / 'chromedriver.exe'
 
 
 def make_chrome_browser(*options: str) -> webdriver.Chrome:
