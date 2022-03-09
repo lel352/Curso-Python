cb3ee759c652c4810a88cfcf84e6337d47e31a43
Leandro <leandro@pcsistemas.com.br>
2022-03-09 15:04:12 -0300

    Aula 08 - Projeto e-commerce - Detalhes do produto

--- Projeto E-commerce/loja/urls.py
+++ Projeto E-commerce/loja/urls.py
@@ -33,5 +33,5 @@ if settings.DEBUG:
         path('__debug__/', include(debug_toolbar.urls)),
     ]
 
-urlpatterns += static(settings.MEDIA_URL, documment_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
